9abfd1f1779a65345c7a1455772523165bc1b8b9
Ruben Arias gomez <ruben.ariasg@est.iudigital.edu.co>
2025-12-14 23:18:44 -0500

    Delete _ _ init_ _.py

--- _ _ init_ _.py
+++
@@ -1,6 +0,0 @@
-# mini_turtle/__init__.py
-
-from .drawer_logic import adelante, abajo, reiniciar
-
-# Define qué se importa con "from mini_turtle import *"
-__all__ = ["adelante", "abajo", "reiniciar"]
