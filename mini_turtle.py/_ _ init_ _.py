224e9115438bb8e341b98eaf55c43ac135c02e48
Ruben Arias gomez <ruben.ariasg@est.iudigital.edu.co>
2025-12-14 23:21:17 -0500

    Add files via upload

---
+++ mini_turtle.py/_ _ init_ _.py
@@ -0,0 +1,6 @@
+# mini_turtle/__init__.py
+
+from .drawer_logic import adelante, abajo, reiniciar
+
+# Define qué se importa con "from mini_turtle import *"
+__all__ = ["adelante", "abajo", "reiniciar"]
